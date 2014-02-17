21e6cef925e86b4f4031b512c95dc8316c88484f
Riccardo Petrocco <r.petrocco@gmail.com>
2014-02-17 14:26:57 +0100

    fixup! added graphs for aggregate (only 1 leecher experiments)

--- experiments/libswift/aggregate_parser.py
+++ experiments/libswift/aggregate_parser.py
@@ -5,58 +5,6 @@ import glob
 import re
 from sys import argv
 
-# parses the stderr output of each libswift client
-def parse_stderr(logDir, outputDir, clientName):
-
-    print >> sys.stderr, "Parsing stderr output of: {0}".format( clientName )
-
-    logfile = os.path.join(logDir, '00000.err')
-    datafile = os.path.join(outputDir, clientName + '.err')
-    if not os.path.exists( logfile ) or not os.path.isfile( logfile ):
-        print >> sys.stderr, "Either the input or the output file is missing"
-        exit(1)
-    if os.path.exists( datafile ):
-        print >> sys.stderr, "Output already present"
-        exit(1)
-        
-    fl = None
-    fd = None
-    try:
-        fl = open( logfile, 'r' )
-        fd = open( datafile, 'w' )
-        fd.write( "time percent upload download\n" )
-        relTime = 0
-        up_bytes = 0
-        down_bytes = 0
-        for line in fl:
-            if line[:5] == 'SLEEP':
-                relTime += 1
-            elif line[:4] == 'done' or line[:4] == 'DONE':
-                # Split over ' ', then over ',', then over '(', then over ')', and keep it all in one array
-                split = reduce( lambda x,y: x + y.split( ')' ), reduce(lambda x,y: x + y.split( '(' ), reduce(lambda x,y: x + y.split( ',' ), line.split( ' ' ), []), []), [])
-                dlspeed = (int(split[16]) - down_bytes) / 1024.0
-                down_bytes = int(split[16])
-                upspeed = (int(split[10]) - up_bytes) / 1024.0
-                up_bytes = int(split[10])
-                                    
-                percent = 0
-                if int(split[3]) > 0:
-                    percent = 100.0 * ( float(int(split[1])) / float(int(split[3])) )
-                    
-                fd.write( "{0} {1} {2} {3}\n".format( relTime, percent, upspeed, dlspeed ) )
-                relTime += 1
-    finally:
-        try:
-            if fd:
-                fd.close()
-        except Exception:
-            pass
-        try:
-            if fl:
-                fl.close()
-        except Exception:
-            pass
-
 
 # parses the ledbat log of each libswift client
 def parse_stderr(logDir, header, seedersfile, leechersfile):
@@ -119,8 +67,8 @@ def check_dirs(inputDir, outputDir):
         print >> sys.stderr, "Please provide a good output directory"
         return
 
-    if outputDir == inputDir and os.path.exists( os.path.join(outputDir,"output") ):
-        outputDir = os.path.join(outputDir,"output")
+    #if outputDir == inputDir and os.path.exists( os.path.join(outputDir,"output") ):
+    #    outputDir = os.path.join(outputDir,"output")
         
     dirs = [ d for d in os.listdir(inputDir) if os.path.isdir( os.path.join(inputDir, d) ) and project_name.match(d) ]
     
